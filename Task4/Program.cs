f3215ec2af6ef80a5a6a54faa490d15e57386975
AndreyVolkov <volk_bur-gora@mail.ru>
2022-06-21 21:39:35 +0300

    четвертая задача

---
+++ Task4/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+string param = Console.ReadLine(); 
+int N = int.Parse(param); 
+
+ if (N > 1)
+ {
+     int a = 2;
+     while (a <= N)
+     {
+         if (a == N || a + 1 == N) 
+         {
+             Console.Write($"{a}");
+             break;
+         }
+         Console.Write($"{a}, ");
+         a = a + 2;
+     }
+ }
+ else
+{ 
+     int a = 0;
+     while (a >= N)
+     {
+         if (a == N || a - 1 == N)
+         {
+             Console.Write($"{a}");
+             break;
+         }
+         Console.Write($"{a}, ");
+         a = a - 2;
+
+     }
+}
